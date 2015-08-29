242f6b0092e0474e76a9ed2d4a65126f85fe3659
Yuri Neves Silveira <yurinevessilveira@gmail.com>
2015-08-28 21:42:20 -0300

    watch plugin

---
+++ test/sample/sample.js
@@ -0,0 +1,30 @@
+// - -------------------------------------------------------------------- - //
+
+"use strict";
+
+var fs = require("fs");
+var assert = require("assert");
+var Crawler = require("bauer-crawler");
+
+var crawler = new Crawler({
+  config: {
+    
+  }
+});
+
+crawler.loadPlugin(__dirname + "/../../");
+
+crawler.start(function() {
+  setTimeout(function() {
+    fs.writeFileSync(__dirname + "/sample.txt",Math.random());
+  },500);
+  return this
+    .promise()
+    .watch(__dirname + "/sample.txt")
+    .then(function(output) {
+      assert.strictEqual(output.event,"change");
+      assert.strictEqual(output.path,__dirname + "/sample.txt");
+    });
+});
+
+// - -------------------------------------------------------------------- - //
